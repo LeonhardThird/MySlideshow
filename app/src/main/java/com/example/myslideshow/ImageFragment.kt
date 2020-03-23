dabccdf67b2efffdf73bc10a69fbe9580bda84d2
LeonhardThird <leonhardthird@gmail.com>
2020-03-23 10:27:49 +0900

    8-3-1; フラグメントとアクティビティの再作成

--- app/src/main/java/com/example/myslideshow/ImageFragment.kt
+++ app/src/main/java/com/example/myslideshow/ImageFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_image.*
 
 /**
  * A simple [Fragment] subclass.
  */
+val IMG_RES_ID = "IMG_RES_ID"
+
 class ImageFragment : Fragment() {
 
     override fun onCreateView(
@@ -18,5 +21,28 @@ class ImageFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_image, container, false)
     }
+    companion object {
+        fun newInstance(imageResourceId: Int): ImageFragment {
+            val bundle = Bundle()
+            bundle.putInt(IMG_RES_ID, imageResourceId)
+            val imageFragment = ImageFragment()
+            imageFragment.arguments = bundle
+            return imageFragment
+        }
+    }
 
+    private var imgResId: Int? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let{
+            imgResId = it.getInt(IMG_RES_ID)
+        }
+    }
+    override fun onActivityCreated(savedInstanceState: Bundle?) { 1
+        super.onActivityCreated(savedInstanceState)
+        imgResId?.let {
+            imageView.setImageResource(it)
+        }
+    }
 }
